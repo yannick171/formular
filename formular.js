676d9bafb2661ce4aa410776175c2f3bdd20e9d6
yannick <43271180+yannick171@users.noreply.github.com>
2018-10-10 16:04:03 +0200

    Add files via upload

---
+++ formular.js
@@ -0,0 +1,65 @@
+function surligne(champ, erreur)
+{
+   if(erreur)
+      champ.style.backgroundColor = "#fba";
+   else
+      champ.style.backgroundColor="#FFFFF";
+}
+function verifPseudo(champ)
+{
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+function verifMail(champ)
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifAge(champ)
+{
+   var age = parseInt(champ.value);
+   if(isNaN(age) || age < 5 || age > 111)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+
+function verifForm(f)
+{
+   var pseudoOk = verifPseudo(f.pseudo);
+   var mailOk = verifMail(f.email);
+   var ageOk = verifAge(f.age);
+   
+   if(pseudoOk && mailOk && ageOk)
+      return true;
+   else
+   {
+      alert("Veuillez remplir correctement tous les champs");
+      return false;
+   }
+}
